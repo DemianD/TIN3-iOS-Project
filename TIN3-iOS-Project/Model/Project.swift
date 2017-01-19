e33f0616e8d677e17ba34111bf955a28bbacb144
Demian Dekoninck <demian.dekoninck@telenet.be>
2017-01-19 16:32:13 +0100

    create computed color value for a project

--- TIN3-iOS-Project/Model/Project.swift
+++ TIN3-iOS-Project/Model/Project.swift
@@ -11,16 +11,36 @@ import ObjectMapper
 
 class Project : Mappable
 {
-    
     var statuses = [
         Status.Ok: "Alles is oké",
         .SslExpired: "Ssl is vervallen",
         .Unreachable: "Website is niet bereikbaar"
     ]
  
+    // Source: https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
+    var colors = [
+        "Red": UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1.0),
+        "Orange": UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1.0),
+        "Yellow": UIColor(red: 255/255, green: 204/255, blue: 0/255, alpha: 1.0),
+        "Green": UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1.0),
+        "TealBlue": UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1.0),
+        "Blue": UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1.0),
+        "Purple": UIColor(red: 88/255, green: 86/255, blue: 214/255, alpha: 1.0),
+        "Pink": UIColor(red: 255/255, green: 45/255, blue: 85/255, alpha: 1.0),
+    ]
+    
     var id = -1;
     var name: String?
     var url: String?
+    var colorName: String?
+    
+    // Computed property
+    var color: UIColor {
+        if let name = colorName {
+            return colors[name]!
+        }
+        return UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1.0)
+    }
     
     var status: Status?
     
@@ -36,6 +56,7 @@ class Project : Mappable
         id <- map["id"]
         name <- map["name"]
         url <- map["url"]
+        colorName <- map["color"]
     }
     
     
@@ -48,5 +69,4 @@ class Project : Mappable
             }
         }
     }
- 
 }
